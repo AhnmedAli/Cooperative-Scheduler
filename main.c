aab649553690c428561e483b30f613c385b7e5c9
a9aly <74613419+a9aly@users.noreply.github.com>
2021-04-09 16:27:54 +0200

    Ambient temperature monitor application
    - Allow user to input threshold temperature through terminal using UART1 and set it as a high priority task of priority 1.
    - Read the ambient temperature using temperature sensor in DS3221 and sit is as a lower priority task of priority 2.
    - When ambient temperature exceed the threshold temperature, the terminal shows a warning message and the led flashes for 5 times. 
    - If the next reading is lower than the temperature threshold the led does not flash and the warning message does not show up.
    - Provide a good user interface to read and display temperatures.

--- main.c
+++ main.c
@@ -51,6 +51,8 @@ typedef struct queue2Node
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+I2C_HandleTypeDef hi2c1;
+
 UART_HandleTypeDef huart1;
 
 /* USER CODE BEGIN PV */
@@ -64,6 +66,7 @@ delayedQueueNode * delayedqueueHead = NULL;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_I2C1_Init(void);
 static void MX_USART1_UART_Init(void);
 /* USER CODE BEGIN PFP */
 /*APIs declaration*/
@@ -76,17 +79,16 @@ void readyQueueInsertNewTask(void (*funcPointer)(void),int priority);
 void delayQueueInsertNewTask(void (*funcPointer)(void),int ticks);
 void delayQueueTickDec (int ticks_counter);
 /* Tasks declaration */
-void printHi(void);
-void printHow(void);
-void printAre(void);
-void printYou(void);
+void Temp(void);
+void TempThreshold(void);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 /*Array of Pointers to tasks functions*/
-void (*tasks_ptr[4])(void)={&printHi,&printHow,&printAre,&printYou};
-
+void (*tasks_ptr[])(void)={&Temp,&TempThreshold};
+uint8_t tempThreshold [3];
+uint8_t buffer;
 /* USER CODE END 0 */
 
 /**
@@ -117,6 +119,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_I2C1_Init();
   MX_USART1_UART_Init();
   /* USER CODE BEGIN 2 */
 	Init();
@@ -175,8 +178,9 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
-  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1;
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART1|RCC_PERIPHCLK_I2C1;
   PeriphClkInit.Usart1ClockSelection = RCC_USART1CLKSOURCE_PCLK2;
+  PeriphClkInit.I2c1ClockSelection = RCC_I2C1CLKSOURCE_PCLK1;
   if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
   {
     Error_Handler();
@@ -187,6 +191,52 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief I2C1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_I2C1_Init(void)
+{
+
+  /* USER CODE BEGIN I2C1_Init 0 */
+
+  /* USER CODE END I2C1_Init 0 */
+
+  /* USER CODE BEGIN I2C1_Init 1 */
+
+  /* USER CODE END I2C1_Init 1 */
+  hi2c1.Instance = I2C1;
+  hi2c1.Init.Timing = 0x10909CEC;
+  hi2c1.Init.OwnAddress1 = 0;
+  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
+  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+  hi2c1.Init.OwnAddress2 = 0;
+  hi2c1.Init.OwnAddress2Masks = I2C_OA2_NOMASK;
+  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
+  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Analogue filter
+  */
+  if (HAL_I2CEx_ConfigAnalogFilter(&hi2c1, I2C_ANALOGFILTER_ENABLE) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Digital filter
+  */
+  if (HAL_I2CEx_ConfigDigitalFilter(&hi2c1, 0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN I2C1_Init 2 */
+
+  /* USER CODE END I2C1_Init 2 */
+
 }
 
 /**
@@ -231,9 +281,21 @@ static void MX_USART1_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin : PB3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
@@ -244,10 +306,9 @@ static void MX_GPIO_Init(void)
 
 void Init(void)
 {
-		HAL_SYSTICK_Config(0x3D08CE);
-		QueTask(tasks_ptr[1],2);
-		QueTask(tasks_ptr[2],3);
-		QueTask(tasks_ptr[3],4);
+		HAL_SYSTICK_Config(0x3D08CE);//79999*50
+		QueTask(tasks_ptr[0],2);
+		QueTask(tasks_ptr[1],1);
 }
 
 void QueTask(void (*funcPointer)(void),int priority)
@@ -381,23 +442,78 @@ void ReRunMe(int tick)
 }
 /**@brief Tasks Initialization
 	*/
-void printHi(void)
+void Temp(void)
 {
-	HAL_UART_Transmit(&huart1,(uint8_t *) "HI",2,100);
+	uint8_t controlRegister[2],statusRegister[2],tempUpperByte[2],tempLowerByte[2];
+	//Registers config
+	statusRegister [0]=0x0F;
+	statusRegister [1]=0x00;
+	controlRegister[0]=0x0E;
+	controlRegister[1]=0x00;
+	tempUpperByte[0]=0x11;
+	tempUpperByte[1]=0x00;
+	tempLowerByte[0]=0x12;
+	tempLowerByte[1]=0x00;
+	//Receive via I2C and forward to UART
+	uint8_t out[] = {'+',0,0,'.',0,0,'°','C','\r','\n'};
+	HAL_I2C_Master_Transmit(&hi2c1, 0xD1, statusRegister, 1, 10);
+	HAL_I2C_Master_Receive(&hi2c1, 0xD1, statusRegister+1, 1, 10);
+	if((statusRegister[1]&0x04) != 0x01)
+	{
+		HAL_I2C_Master_Receive(&hi2c1, 0xD1, controlRegister, 1, 10);
+		controlRegister[1]=controlRegister[1]|0x20;
+		HAL_I2C_Master_Transmit(&hi2c1, 0xD0, controlRegister, 1, 10);
+		HAL_I2C_Master_Transmit(&hi2c1, 0xD0, tempUpperByte, 1, 10);
+		HAL_I2C_Master_Receive(&hi2c1, 0xD1, tempUpperByte+1, 1, 10);
+		HAL_I2C_Master_Transmit(&hi2c1, 0xD0, tempLowerByte, 1, 10);
+		HAL_I2C_Master_Receive(&hi2c1, 0xD1, tempLowerByte+1, 1, 10);
+		if((tempUpperByte[1]>>7)&0x01) out[0] = '-'; else out[0] = '+';
+		out[1]=tempUpperByte[1]&0x7F;
+		out[4]=25*(tempLowerByte[1]>>6);
+		if(out[1]/10!=0)
+		{
+			out[2]=out[1]%10+'0';
+			out[1]=out[1]/10+'0';
+		}
+		else
+		{
+			out[2]=out[1]+'0';
+			out[1]=0+'0';
+		}
+		if(out[4]/10!=0)
+		{
+			out[5]=out[4]%10+'0';
+			out[4]=out[4]/10+'0';
+		}
+		else
+		{
+			out[5]=out[4]+'0';
+			out[4]=0+'0';
+		}
+		// transmit temp to UART
+		HAL_UART_Transmit(&huart1,(uint8_t*) "\n\rCurrent Temprature is: ",25, 10);
+		HAL_UART_Transmit(&huart1,out, sizeof(out), 10);
+		if(out[0]<=tempThreshold[0] && out[1]>=tempThreshold[1] && out[2]>=tempThreshold[2])
+		{
+			HAL_UART_Transmit(&huart1,(uint8_t*) "\n\rWARNING!! Temp EXCEDDED THE TEMP THRESHOLD!!\n",48, 10);
+			for(int i=0;i<10;i=i+1)
+			{
+				HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_3);
+				HAL_Delay(5);
+			}
+		}
+		ReRunMe(600);
+	}
 }
-void printHow (void)
-{
-	HAL_UART_Transmit(&huart1,(uint8_t *) "How",3,100);
-	ReRunMe(10);
-}
-void printAre (void)
-{
-	HAL_UART_Transmit(&huart1,(uint8_t *) "Are",3,100);
-	ReRunMe(50);
-}
-void printYou (void)
-{
-	HAL_UART_Transmit(&huart1,(uint8_t *) "You",3,100);
+void TempThreshold(void)
+{	
+	HAL_UART_Transmit(&huart1,(uint8_t*)"\n\rEnter Your temp Threshold ", 28, 10);
+	for(int i=0;i<3;i=i+1)
+	{
+		HAL_UART_Receive(&huart1,&buffer, sizeof(buffer), HAL_MAX_DELAY);
+		tempThreshold[i]=buffer;
+	}
+	
 }
 /* USER CODE END 4 */
 
